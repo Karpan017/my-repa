c89bb3e81ceecb6619cd7c6a9aa92a7cdd5525fe
Karpan017 <karpov311097@gmai.com>
2019-08-16 20:31:29 +0400

    Сделал ДЗ по прошлому уроку , я шел подряд и каждая программа идет через пробел . По всем трем ссылкам . Если не удобно оформление , просьба указать ваши комментарии . С Уважением , Карпов Денис

---
+++ lesson_1 HomeWork.py
@@ -0,0 +1,56 @@
+#a = 5
+#b = 7
+#c = 12
+#print (a + b * c)
+
+
+
+#number = int(input ('Введите число : '))
+#print ('я прибавил два ' , number + 2)
+
+
+
+
+#a = 18
+#age = int(input('Введите ваш возраст : '))
+#if age >= a:
+#    print('Доступ разрешен ')
+#else:
+#    print('доступ закрыт ')
+
+
+#number = int(input ('Введите число от 1 до 10 : '))
+#while number < 1 or number > 10:
+#     print ('Не правильно , введите верное число ')
+#     number = int(input('Введите число от 1 до 10 : '))
+#else: print (number ** 2)
+
+
+
+
+
+#a = int(input('Введите первое число : '))
+#b = int(input('Введите второе число : '))
+#print (b,a)
+
+
+
+
+
+
+
+
+#a = input('Ваше имя : ')
+#b = input('Ваша Фамилия : ')
+#age = int(input('Введите ваш возраст : '))
+#weight = int(input('Введите ваш вес : '))
+#if age <= 30 and weight < 50 or weight < 120:
+#    print(b, a,' ,Да ты бодрячком')
+#elif age < 15 or age > 30 and weight < 30 :
+#    print(b, a,',У вас анорексия !!! Идите к врачу , в срочном поряке !!!')
+#elif age >=30 and weight < 50 or weight > 120:
+#    print(b, a,',Брось булку и вали в ЗАЛ !!!!!')
+#elif age >=40 and weight < 50 or weight > 120:
+#    print(b, a,' ,Ты как еще живешь ? ')
+#elif age > 70 and weight < 50 or weight > 120:
+#   print(b, a, ' ,А вы крепкий орешек )) ')
