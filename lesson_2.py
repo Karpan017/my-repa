4e9a7578c3fe2fa066ddae0f2682f40f88275021
Karpan017 <karpov311097@gmai.com>
2019-08-20 16:57:59 +0400

    задание к второму уроку , сделал только легкое , очень слабо разобрался с темой

---
+++ lesson_2.py
@@ -0,0 +1,32 @@
+#Fruits = ['Яблоко', 'Банан', 'Киви', 'Арбуз', 'Манго', 'Груша', 'Помидор', 'Дыня']
+#last_name = len(Fruits)
+#for i in range(last_name):
+#print(str(i + 1) + '.' + '{}'.format(Fruits[i]))
+
+
+
+#b = {'a', 'b', 'c', 'c', 'a', 'e'}
+#d = {'a', 'b', 'c', 'd'}
+#c = b - d
+#print(list(c))
+
+
+
+
+
+#first_list = [2, 7, 5, 6, 9, 15]
+#new_list = []
+#last_name = len(first_list)
+#for i in range(last_name):
+#     if first_list[i] % 2 == 0:
+#      new_list.append(first_list[i] / 4)
+#else:
+#      new_list.append(first_list[i] * 2)
+#print(new_list)
+
+
+
+
+
+
+
